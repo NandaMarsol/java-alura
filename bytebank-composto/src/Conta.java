d1f0c2d1649f2fcba95c8cf596b9e39e88c58964
NandaMarsol <fernanda.almferreira@gmail.com>
2021-07-13 22:14:07 -0300

    :tada: subindo os exercicios dos cursos Java Parte I e Java Parte II

---
+++ bytebank-composto/src/Conta.java
@@ -0,0 +1,39 @@
+
+
+public class Conta {
+	
+		// atributos da classe
+		double saldo;
+		int agencia;
+		int numero;
+		Cliente titular;
+		
+		// métodos - definem o comportamento de um objeto 
+		
+		// void é quando não tem / não precisa de retorno
+		public void deposita(double valor){ // recebe valor do tipo double
+			//this.saldo = this.saldo + valor; // usamos o this dentro de um método para acessar um atributo
+			this.saldo += valor;
+		}
+		
+		public boolean saca(double valor){
+			if(this.saldo >= valor){
+				//this.saldo = this.saldo - valor;
+				this.saldo -= valor;
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public boolean transfere(double valor, Conta destino){
+			if(this.saca(valor)){
+			//if(this.saldo >=valor) {	
+				//this.saldo -= valor;
+				destino.deposita(valor);
+				return true;
+			} else {
+				return false;
+			}
+		}
+}
