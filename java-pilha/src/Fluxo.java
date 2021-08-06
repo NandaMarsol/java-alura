ab5d94a1a9524d4523d4a287b454a4f8e3f7e494
NandaMarsol <fernanda.almferreira@gmail.com>
2021-08-06 18:59:49 -0300

    :construction: Exceções - Tratamento de exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,15 +1,23 @@
+// Classe para demonstrar o tratamento de exceções nos métodos com o bloco try-catch
 
 public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+        	metodo1();
+        } catch (ArithmeticException | NullPointerException exception) { // tratando múltiplas exceções com o pipe |
+        	String msg = exception.getMessage(); // pegando a mensagem da exceção
+            System.out.println("Exception " + msg);
+            exception.printStackTrace(); // pegando o rastro da nossa exceção "stacktrace"
+        }
+        
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        	metodo2();    
         System.out.println("Fim do metodo1");
     }
 
@@ -17,6 +25,14 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            Conta c = null;
+            c.deposita(); // testando a classe Conta
+ //           try { // tratamento da exceção para evitar que caia na pilha de execução 
+ //           	int a = i / 0; // forçando uma exceção ArithmeticException
+ //           } catch (ArithmeticException ex) {
+ //           	System.out.println("Exceção ArithmeticException");
+ //           }
+            
         }
         System.out.println("Fim do metodo2");        
     }
