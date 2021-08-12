9b95a33c8b754d8bfe7049f76409b93d0591d1dd
NandaMarsol <fernanda.almferreira@gmail.com>
2021-08-11 22:56:19 -0300

    Alterando o tipo do Array de Referencias

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,23 +1,44 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
 		// criando Array com cinco posições para guardar contas
-		ContaCorrente[] contas = new ContaCorrente[5];
+        Conta[] contas = new Conta[5]; // alterando o tipo
 		
-		// instanciando duas contas e guardando-as nas duas primeiras posições do Array
+     // cria instância de ContaCorrente
 		ContaCorrente cc1 = new ContaCorrente(44, 11);
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-
-        contas[0] = cc1;
-        contas[1] = cc2;
+		contas[0] = cc1;
+		
+		// cria instância de ContaPoupanca
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        contas[1] = cc2; 
         
         // acessando a conta da primeira posição e imprimindo
-        System.out.println(contas[0].getAgencia());
+        System.out.println(contas[1].getNumero());
+
+        // não compila
+ //     ContaCorrente ref = contas[0]; // Ao mudarmos o tipo do array contas de ContaCorrente, a seguinte instrução deixa de compilar
+        // Porque em tempo de execução, temos um objeto do tipo ContaCorrente como primeiro elemento do array contas, sua referência é do tipo Conta quando acessarmos contas[0]. 
+        // Sendo assim, uma solução é alterarmos o tipo da variável ref para Conta:
+        
+     // alterou o tipo, agora compila
+        Conta ref = contas[0];
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
 	}
 
 }
+
+
+/* - uma array do tipo Object pode guardar qualquer tipo de referência
+   - quando convertemos uma referência genérica para uma referência mais específica é preciso usar um type cast
+   - o cast só compila quando é possível, mesmo assim pode falhar na hora de rodar
+   - quando o type cast falha podemos receber uma ClassCastException
+   - para receber valores ao chamar o programa Java na linha de comando podemos usar o array String[] no método main
+*/
