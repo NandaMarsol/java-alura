296b2829d12525abe7949fa97605140ecafef74d
NandaMarsol <fernanda.almferreira@gmail.com>
2021-09-30 18:43:19 -0300

    Streams e Collectors

--- java8/src/java8/TestaCurso.java
+++ java8/src/java8/TestaCurso.java
@@ -3,6 +3,9 @@ package java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestaCurso {
@@ -45,6 +48,29 @@ public class TestaCurso {
 		   .filter(c -> c.getAlunos() > 50)
 		   .map(Curso::getAlunos)
 		   .forEach(System.out::println);
+		
+		// pegar o primeiro elemento desse Stream filtrado
+		// Optional oferece uma variedade imensa de novos métodos que nos permite trabalhar de forma funcional com nossos valores, tirando maior proveito dos novos recursos de default methods, lambdas e method reference
+		Optional<Curso> optionalCurso = cursos.stream() 
+		   .filter(c -> c.getAlunos() > 50)
+		   .findFirst();
+		
+		Curso curso = optionalCurso.orElse(null);
+		System.out.println(curso.getNome());
+		
+		// calculando a quantidade média de alunos de todos os cursos
+		cursos.stream() 
+	    .mapToInt(c -> c.getAlunos())
+	    .average()
+		.ifPresent(System.out::println);
+		
+		
+		// coletando o resultado do stream em uma List
+		List<Curso> cursosFiltrados = cursos.stream()
+				   .filter(c -> c.getAlunos() > 50)
+				   .collect(Collectors.toList());
 	}
+	
+		
 
 }
