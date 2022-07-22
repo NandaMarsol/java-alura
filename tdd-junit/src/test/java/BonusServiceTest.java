de42368f6cd511f74ab663afdc9301bbcadc1030
NandaMarsol <fernanda.almferreira@gmail.com>
2022-07-21 23:29:37 -0300

    Criando teste para a classe de Bonus

---
+++ tdd-junit/src/test/java/BonusServiceTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+
+public class BonusServiceTest {
+
+	@Test
+	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Luna", LocalDate.now(), new BigDecimal("25000")));
+		
+		assertEquals(BigDecimal.ZERO, bonus);
+
+	}
+	
+	@Test
+	void bonusDeveriaSer10PorCentoDoSalario() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Luna", LocalDate.now(), new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.0"), bonus);
+
+	}
+
+}
